fa7ea1ccacf27fb656b15299f5663566964f25df
jlmansilla <juanmancillaster@gmail.com>
2023-02-13 21:35:46 -0300

    error en comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
+  resources :posts 
   resources :comments
-  resources :posts
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
